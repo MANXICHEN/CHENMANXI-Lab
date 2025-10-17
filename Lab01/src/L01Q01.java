4bb6aad407874ba381af4f4199467a841a399ef4
CHEN MANXI <24233146@siswa.um.edu.my>
2025-10-17 19:05:41 +0800

    Submit Lab 1 Question 1

---
+++ Lab01/src/L01Q01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 陈满熙
+ */
+public class L01Q01 {
+    public class L01Q01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
+
+    
+}
